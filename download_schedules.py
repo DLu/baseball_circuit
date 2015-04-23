2887a2162322f12dd00e41607c8d932d04f00d63
David Lu <davidvlu@gmail.com>
2015-04-22 23:09:58 -0500

    Updated download schedules

--- download_schedules.py
+++ download_schedules.py
@@ -3,44 +3,73 @@
 from datetime import datetime, timedelta
 import calendar
 import urllib2
+import csv
+from teams import *
 
-YEAR = 2013
+YEAR = 2015
 
-NAMES = {'D-backs': 'ARI', 'Braves': 'ATL', 'Orioles': 'BAL', 'Red Sox': 'BOS', 'Cubs': 'CHC', 'Reds': 'CIN', 'Indians': 'CLE', 'Rockies': 'COL', 'White Sox': 'CWS', 'Tigers': 'DET', 'Astros': 'HOU', 'Royals': 'KC', 'Dodgers': 'LA', 'Angels': 'LAA', 'Marlins': 'MIA', 'Brewers': 'MIL', 'Twins': 'MIN', 'Mets': 'NYM', 'Yankees': 'NYY', 'Athletics': 'OAK', 'Phillies': 'PHI', 'Pirates': 'PIT', 'Padres': 'SD', 'Mariners': 'SEA', 'Giants': 'SF', 'Cardinals': 'STL', 'Rays': 'TB', 'Rangers': 'TEX', 'Blue Jays': 'TOR', 'Nationals': 'WAS'}
+def get_datetime(date, time):
+    if time == '03:33 AM':
+        hour = 19
+        mins = 0
+    else:
+        hour = int(time[:2])
+        mins = int(time[3:5])
+    if 'PM' in time and hour!=12:
+        hour += 12
+    t = datetime(YEAR, int(date[:2]), int(date[3:5]), hour, mins)
+    return calendar.timegm(t.utctimetuple())
 
-def read_schedule(csv):
-    header = None
+def read_schedule(file_obj, remove_spring=True):
     games = []
-    for line in csv.split('\n'):
-        row = line.split(',')
-        if header is None:
-            header = row
-        else:
-            (date, time, teams) = (row[0], row[2], row[3])
-            i = teams.index(' at ')
-            if 'All-Stars' in teams:
-                continue
-            away = NAMES[teams[:i].strip()]
-            home = NAMES[teams[i+3:].strip()]
-            if time == '03:33 AM':
-                hour = 19
-                mins = 0
-            else:
-                hour = int(time[:2])
-                mins = int(time[3:5])
-            if 'PM' in time and hour!=12:
-                hour += 12
-            t = datetime(YEAR, int(date[:2]), int(date[3:5]), hour, mins)
-            stamp = calendar.timegm(t.utctimetuple())
-            games.append( (stamp, away, home) )
+    venues = []
+    
+    for data in csv.DictReader(file_obj, delimiter=','):
+        date = data['START_DATE']
+        time = data['START_TIME_ET']
+        teams = data['SUBJECT']
+        
+        if 'All-Stars' in teams:
+            continue
+
+        away_name, _, home_name = teams.partition(' at ')
+        away = lookup_by_name(away_name)
+        home = lookup_by_name(home_name)
+        
+        if away is None or home is None:
+            print 'Skipping %s @ %s (%s)'%(away_name, home_name, date)
+            continue
+            
+        stamp = get_datetime(date, time)    
+        
+        games.append( (stamp, away, home) )
+        venues.append( data['LOCATION'] )
+        
+    if remove_spring:
+        season = []
+        final = venues[-1]
+        for game, stadium in zip(games, venues):
+            if stadium == final:
+                season.append(game)
+        games = season        
+        
     return games
 
+def get_filename(number, year):
+    return 'http://mlb.mlb.com/soa/ical/schedule.csv?home_team_id=%d&season=%d'%(number, year)
 
-schedule = []
+def get_games(number, year):
+    response = urllib2.urlopen( get_filename(number, year) )
+    return read_schedule(response)
 
-for num in [109, 144, 110, 111, 112, 113, 114, 115, 145, 116, 117, 118, 119, 108, 146, 158, 142, 121, 147, 133, 143, 134, 135, 136, 137, 138, 139, 140, 141, 120]:
-    response = urllib2.urlopen('http://mlb.mlb.com/soa/ical/schedule.csv?home_team_id=%d&season=%d'%(num, YEAR))
-    schedule += read_schedule(response.read())
-
-for x in sorted(schedule):
-    print '%d\t%s\t%s'%x
+if __name__=='__main__':
+    import argparse    
+    parser = argparse.ArgumentParser(description='Schedule Downloader')
+    parser.add_argument('year', type=int)
+    parser.add_argument('teams', metavar='team', nargs='*')
+    args = parser.parse_args()
+    if len(args.teams)==0:
+        args.teams = all_teams()
+    
+    for team in args.teams:
+        print get_games( get_mlb_num(team), args.year )
